1c79f05977beed21b19ad1a70a8f060930982234
Ghadahss <Ghadah.s.alzahrani@gmail.com>
2022-12-06 15:23:39 +0300

    Initial Commit

---
+++ sanse/sanseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sanseApp.swift
+//  sanse
+//
+//  Created by Ghadah on 12/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct sanseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
